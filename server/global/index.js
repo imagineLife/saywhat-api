4ab4794ec8e1e61c76a9fa3268a105fefe26a900
Eric Laursen <ericjlaursen@gmail.com>
2022-01-31 06:47:45 -0500

    events and state

---
+++ server/global/index.js
@@ -0,0 +1,7 @@
+const GLOBAL_STATE = require('./state')
+const ServicesEmitter = require('./server-events');
+
+module.exports = { 
+  GLOBAL_STATE,
+  ServicesEmitter
+}
---
+++ server/global/state.js
@@ -0,0 +1,5 @@
+const GLOBAL_STATE = {
+  DB_CONNECT:false
+}
+
+module.exports = GLOBAL_STATE;
