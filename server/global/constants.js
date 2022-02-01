c8be5682c44426e6157c342f94e7e170450c6e01
Eric Laursen <ericjlaursen@gmail.com>
2022-01-31 20:25:18 -0500

    constants

---
+++ server/global/constants.js
@@ -0,0 +1,10 @@
+const routes = {
+  HEALTH_CHECK: '/health-check',
+  DB: {
+    KILL: '/kill',
+    RESTART:'/restart',
+    ROOT: '/db'
+  }
+}
+
+module.exports = routes;
--- server/global/index.js
+++ server/global/index.js
@@ -1,7 +1,8 @@
 const GLOBAL_STATE = require('./state')
 const ServicesEmitter = require('./events');
-
+const routes = require('./constants')
 module.exports = { 
   GLOBAL_STATE,
-  ServicesEmitter
+  ServicesEmitter,
+  routes
 }
