91b06cc6071e62375d532295f08845d446c97a1d
Eric Laursen <ericjlaursen@gmail.com>
2022-02-01 20:36:30 -0500

    init test file

---
+++ server/routes/healthcheck/health-check.spec.js
@@ -0,0 +1,22 @@
+// dependencies
+const chai= require('chai');
+const chaiHttp= require('chai-http');
+const { HEALTH_CHECK } = require('./../../global/constants');
+const { expressObj } = require('./../../express-setup');
+const { startServer, stopServer } = require('./../../../server');
+describe(HEALTH_CHECK, function () {
+  chai.use(chaiHttp);
+  let localServerObj;
+  beforeAll(async function () {
+    localServerObj = await startServer(expressObj)
+  });
+
+  afterAll(async function () {
+    await stopServer(localServerObj)
+  });
+
+  it('get returns "server is up & running!"', async function () {
+    const res = await chai.request(localServerObj).get(HEALTH_CHECK);
+    expect(res.text).toBe("server is up & running!");
+  });
+});
