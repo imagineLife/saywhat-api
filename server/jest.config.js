266b6964734410c2ff2e94c0497ffd63ac046997
Eric Laursen <ericjlaursen@gmail.com>
2022-02-01 21:07:19 -0500

    attempt moving & importing jest config from pkg to file

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,10 +1,5 @@
+const { jest : { coverageThreshold } } = require('./package.json');
+
 module.exports = {
-  coverageThreshold: {
-    global: {
-      statements: 67.74,
-      branches: 50,
-      functions: 30.76,
-      lines: 67.74
-    }
-  }
+  coverageThreshold
 }
