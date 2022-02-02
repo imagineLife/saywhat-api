b6fd8d5b70910c06dc246825b97df29285089a04
Eric Laursen <ericjlaursen@gmail.com>
2022-02-01 21:15:23 -0500

    remove conditions

--- server/index.js
+++ server/index.js
@@ -6,10 +6,8 @@ const { expressObj } = require('./express-setup');
 
 */ 
 const PORT = process.env.PORT || 3000;
-if(process.env.NODE_ENV !== 'test'){
-  console.log(`PROCESS PORT: ${process.env.PORT}`)
-  console.log(`PROCESS NODE_ENV: ${process.env.NODE_ENV}`)
-}
+console.log(`PROCESS PORT: ${process.env.PORT}`)
+console.log(`PROCESS NODE_ENV: ${process.env.NODE_ENV}`)
 
 
 
@@ -22,9 +20,7 @@ if(process.env.NODE_ENV !== 'test'){
 */ 
 // let serverApp;
 async function stopServer(srvr){
-  if(process.env.NODE_ENV !== 'test'){
-    console.log('CLOSING SERVER')  
-  }
+  console.log('CLOSING SERVER')  
   return await srvr.close(() => {
     if (require.main === module) {
       console.log('HTTP Graceful Shutdown')
@@ -38,9 +34,7 @@ function startServer(srvr){
   })
 
   return srvr.listen(PORT, () => {
-    if(process.env.NODE_ENV !== 'test'){
-      console.log(`http server listening on ${PORT}`)
-    }
+    console.log(`http server listening on ${PORT}`)
   })
 }
 
