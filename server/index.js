cc59771d5078e210f7b17cd411c733e8c0146855
Eric Laursen <ericjlaursen@gmail.com>
2022-02-01 21:14:00 -0500

    add missing env var

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const { expressObj } = require('./express-setup');
 
 */ 
 const PORT = process.env.PORT || 3000;
-if(process.env !== 'test'){
+if(process.env.NODE_ENV !== 'test'){
   console.log(`PROCESS PORT: ${process.env.PORT}`)
   console.log(`PROCESS NODE_ENV: ${process.env.NODE_ENV}`)
 }
@@ -22,7 +22,7 @@ if(process.env !== 'test'){
 */ 
 // let serverApp;
 async function stopServer(srvr){
-  if(process.env !== 'test'){
+  if(process.env.NODE_ENV !== 'test'){
     console.log('CLOSING SERVER')  
   }
   return await srvr.close(() => {
@@ -38,7 +38,7 @@ function startServer(srvr){
   })
 
   return srvr.listen(PORT, () => {
-    if(process.env !== 'test'){
+    if(process.env.NODE_ENV !== 'test'){
       console.log(`http server listening on ${PORT}`)
     }
   })
