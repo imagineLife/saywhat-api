2d490ded9bcba82575d3ee14c28880a68229d620
Eric Laursen <ericjlaursen@gmail.com>
2022-02-03 06:36:31 -0500

    inc coverage, test start/stop server

--- server/index.spec.js
+++ server/index.spec.js
@@ -1,5 +1,34 @@
-describe('mock test one', () => {
-  it('runs jest successfully', () => {
-    expect(1).toBe(1)
+const { startServer, stopServer } = require('./')
+describe('Server index', () => {
+  jest.mock('./helpers');
+  const { twoAreEqual } = require('./helpers');
+
+  describe('mocking twoAreEqual (jest mock test)', () => {
+    twoAreEqual.mockImplementation(() => true)
+    it('returns tru from (false,true)', () => {
+      expect(twoAreEqual(false, true)).toBe(true)
+    })
+  })
+
+  describe('startServer calls expressServer.listen', () => {
+    it('calls server.listen with default port', () => {
+      const mockListen = jest.fn();
+      let mockServer = {
+        listen: mockListen
+      }
+      startServer(mockServer)
+      expect(mockListen).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('stopServer calls expressServer.close', () => {
+    it('calls server.listen with default port', () => {
+      const mockCloseFn = jest.fn();
+      let closeServerMock = {
+        close: mockCloseFn
+      }
+      stopServer(closeServerMock)
+      expect(mockCloseFn).toHaveBeenCalledTimes(1)
+    })
   })
 })
