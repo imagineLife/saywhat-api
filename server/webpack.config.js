fad78cc406379a5f3e2ac4bf9b156751bea23f9b
Eric Laursen <ericjlaursen@gmail.com>
2022-02-06 17:15:23 -0500

    init wp config

---
+++ server/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = (env) => {  
+  console.log('webpackmodule.exports vars')
+  console.log(env)
+  
+  return {
+    entry: './index.js',
+    output: {
+      filename: 'server-bundle.js',
+      path: path.resolve(__dirname, `dist${env?.mode === 'development' ? '/dev' : ''}`)
+    },
+    target: 'node',
+    // externals: [nodeExternals()],
+    mode: env?.mode || 'production'
+  }
+}
