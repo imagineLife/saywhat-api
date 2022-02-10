42b9b28b2e95093b2629722e70de88a4f497bcb1
Eric Laursen <ericjlaursen@gmail.com>
2022-02-10 06:49:06 -0500

    comment db connect

--- server/index.js
+++ server/index.js
@@ -9,13 +9,13 @@ const { connectDB } = require('./db')
 if (twoAreEqual(require.main, module)) {
   try{
     startServer(expressObj)
-    connectDB({
-      username: process.env.MONGO_DB_USER,
-      pw: process.env.MONGO_DB_PW,
-      host: process.env.MONGO_DB_HOST,
-      port: process.env.MONGO_DB_PORT,
-      authDB: process.env.MONGO_DB_AUTH_DB
-    })
+    // connectDB({
+    //   username: process.env.MONGO_DB_USER,
+    //   pw: process.env.MONGO_DB_PW,
+    //   host: process.env.MONGO_DB_HOST,
+    //   port: process.env.MONGO_DB_PORT,
+    //   authDB: process.env.MONGO_DB_AUTH_DB
+    // })
   }catch(e){
     console.log(e)
   }
