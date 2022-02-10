c03d030f6160b091594f5900445c5e45c7649065
Eric Laursen <ericjlaursen@gmail.com>
2022-02-10 08:03:45 -0500

    more config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,48 @@
 const HtmlPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  plugins: [
-    new HtmlPlugin()
-  ]
+module.exports = (env, args) => {
+  const config = {
+    mode: env.mode,
+    module: {
+      rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/react"]
+              }
+            }
+          },
+          {
+            test: /\.html$/,
+            use: [
+              {
+                loader: "html-loader",
+                options: { minimize: true }
+              }
+            ]
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+          }
+        ]
+    },
+    plugins: [
+      new HtmlPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ]
+  };
+  return config
 }
