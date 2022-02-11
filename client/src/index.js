79ff981a6e72a211039c05f666241b445bc698f3
Eric Laursen <ericjlaursen@gmail.com>
2022-02-11 07:32:07 -0500

    linted

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Header = () => <h1>Header Here </h1>
+function Header() {
+  return <h1>Header Here</h1>;
+}
 
-ReactDOM.render(
-    <Header />,
-  document.getElementById("root")
-);
+ReactDOM.render(<Header />, document.getElementById("root"));
