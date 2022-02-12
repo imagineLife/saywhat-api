d3e6309e62b64e958473d2daecfa2985b49a0f85
Eric Laursen <ericjlaursen@gmail.com>
2022-02-11 20:23:30 -0500

    convert js to tsx file

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-function Header() {
-  return <h1>Header Here</h1>;
-}
-
-ReactDOM.render(<Header />, document.getElementById("root"));
---
+++ client/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const Header = (): JSX.Element => (
+  <h1>Sauce Magic</h1>
+)
+
+ReactDOM.render(<Header />, document.getElementById("root"));
