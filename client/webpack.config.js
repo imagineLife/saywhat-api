f92a815cdb865abe7838ce579eef6747810b6842
Eric Laursen <ericjlaursen@gmail.com>
2022-02-11 20:19:46 -0500

    typescript

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,48 +1,59 @@
-const HtmlPlugin = require('html-webpack-plugin')
+const HtmlPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = (env, args) => {
   const config = {
     mode: env.mode,
     module: {
       rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: ["@babel/react"]
-              }
-            }
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          options: {
+            configFile: path.resolve(__dirname, "./.tsconfig"),
           },
-          {
-            test: /\.html$/,
-            use: [
-              {
-                loader: "html-loader",
-                options: { minimize: true }
-              }
-            ]
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/react"],
+            },
           },
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-              // Creates `style` nodes from JS strings
-              "style-loader",
-              // Translates CSS into CommonJS
-              "css-loader",
-              // Compiles Sass to CSS
-              "sass-loader",
-            ],
-          }
-        ]
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: true },
+            },
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
     },
     plugins: [
       new HtmlPlugin({
         template: "./src/index.html",
-        filename: "./index.html"
-      })
-    ]
+        filename: "./index.html",
+      }),
+    ],
   };
-  return config
-}
+  return config;
+};
