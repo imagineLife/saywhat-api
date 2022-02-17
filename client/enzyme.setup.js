cea0509baa3a03cf8f818dc8a3a8cc6a27a52f08
Eric Laursen <ericjlaursen@gmail.com>
2022-02-17 08:14:13 -0500

    enzyme setup file

---
+++ client/enzyme.setup.js
@@ -0,0 +1,8 @@
+import 'regenerator-runtime/runtime';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+// mock fetch in jest tests
+global.fetch = require('jest-fetch-mock');
+
+Enzyme.configure({ adapter: new Adapter() });
