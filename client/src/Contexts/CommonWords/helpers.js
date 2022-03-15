fa177897dead97d1fd3f0f962c0fdbc8bfb9a7b6
Eric Laursen <ericjlaursen@gmail.com>
2022-03-15 15:36:02 -0400

    lint + dfault return

--- client/src/Contexts/CommonWords/helpers.js
+++ client/src/Contexts/CommonWords/helpers.js
@@ -1,10 +1,13 @@
-const sortByWordAlpha = (a, b) => {
+function sortByWordAlpha(a, b): [ string | {word: string}]{
   if (typeof a === 'string') {
     if (a < b) { return -1; }
     if (a > b) { return 1; }
   }
   if (a.word < b.word) { return -1; }
   if (a.word > b.word) { return 1; }
-};
 
-  export { sortByWordAlpha };
+  return 1;
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { sortByWordAlpha };
