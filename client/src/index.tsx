4f55c4a71db808792bf19d996498fdcc942583c1
Eric Laursen <ericjlaursen@gmail.com>
2022-03-17 09:40:12 -0400

    text update

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 const Header = (): JSX.Element => (
-  <h1>Sauce Change-for-pipeline</h1>
+  <h1>Sauce Change-for-pipeline again</h1>
 )
 
 ReactDOM.render(<Header />, document.getElementById("root"));
