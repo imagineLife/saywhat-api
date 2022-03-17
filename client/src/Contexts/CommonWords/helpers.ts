35c220a467001aab27db1f44732e279150ef0403
Eric Laursen <ericjlaursen@gmail.com>
2022-03-17 12:56:08 -0400

    stronger type def + assurance

--- client/src/Contexts/CommonWords/helpers.ts
+++ client/src/Contexts/CommonWords/helpers.ts
@@ -1,15 +1,10 @@
-function sortByWordAlpha(a: string | { word: string}, b: string | { word: string }): number{
+type StringOrWordObj = string | { word: string };
+function sortByWordAlpha(a: StringOrWordObj, b: StringOrWordObj): number{
 
   // bail on bad types
-  const badAType = typeof a !== 'string' && (
-    typeof a === 'object' && 
-    !a.word
-  );
-   const badBType = typeof b !== 'string' && (
-    typeof b === 'object' && 
-    !b.word
-  );
-  if(badAType && badBType){
+  const badAType = typeof a !== 'string' && !a.word
+   const badBType = typeof b !== 'string' &&  !b.word
+  if(badAType || badBType){
     throw new Error('check input on sortByWordAlpha');
   }
 
@@ -18,7 +13,10 @@ function sortByWordAlpha(a: string | { word: string}, b: string | { word: string
     if (a < b) { return -1; }
     return 1
   }
+    
+  if(typeof a === "object" && "word" in a && typeof b === "object" && "word" in b){
     return a.word < b.word ? -1 : 1;
+  }
 }
 
 // eslint-disable-next-line import/prefer-default-export
