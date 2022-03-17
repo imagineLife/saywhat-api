c47a1c0fa8c8a22092a9824b4f36baa9387ff32d
Eric Laursen <ericjlaursen@gmail.com>
2022-03-17 09:25:29 -0400

    text change

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 const Header = (): JSX.Element => (
-  <h1>Sauce Magic</h1>
+  <h1>Sauce Change-for-pipeline</h1>
 )
 
 ReactDOM.render(<Header />, document.getElementById("root"));
