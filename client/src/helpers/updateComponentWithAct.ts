3eef3ee5dba8e0eb6a719a6985ce9485a239db0b
Eric Laursen <ericjlaursen@gmail.com>
2022-03-23 11:35:36 -0400

    js to ts

--- client/src/helpers/updateComponentWithAct.ts
+++ client/src/helpers/updateComponentWithAct.ts
@@ -1,6 +1,7 @@
+import { ReactWrapper } from 'enzyme';
 import { act } from 'react-dom/test-utils';
 
-const updateComponentWithAct = (wrapper) => {
+const updateComponentWithAct = (wrapper: ReactWrapper): void => {
   act(() => {
     // await new Promise((resolve) => { setTimeout(resolve, 0); });
     wrapper.update();
