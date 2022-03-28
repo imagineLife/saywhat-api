1f7b62c5dd26ab26036cc4ea06a4be3b6938a853
Eric Laursen <ericjlaursen@gmail.com>
2022-03-28 14:44:34 -0400

    adjust and increase test coverage

--- server/models/db/db-model.spec.js
+++ server/models/db/db-model.spec.js
@@ -1,7 +1,61 @@
 const { DB } = require('.');
+const { MongoClient } = require('mongodb');
 
-describe('DB Model', () => { })
-  it('sets client and db from constructor props', () => {
-    const Horse = new DB({mongoClient:'asdf', db: 'qwer'})
-    expect(JSON.stringify(Horse)).toBe(JSON.stringify({client:'asdf', db: 'qwer'}))
+describe('DB Model', () => {
+  describe('connect method', () => {
+    describe('logs error strings without expected connection param', () => {
+      beforeEach(() => {
+          delete process.env.MONGO_AUTH;
+        })
+        it('authDB', () => {
+          jest.spyOn(global.console, 'log')
+          const MockDB = new DB({
+            connectionObj: {
+              host: 'localhost',
+              // port: '27017'
+            }, db: 'TestSayWhat'
+          })
+          MockDB.connect()
+          expect(console.log).toHaveBeenCalledWith('Cannot create db connection with missing param')
+        })
+    })
+    describe('methods', () => {
+      let MockDB = new DB({
+        connectionObj: {
+          host: 'localhost',
+          port: '27017'
+        }, dbName: 'TestSayWhat'
+      })
+      
+      beforeAll(() => {
+        jest.spyOn(global.console, 'table')
+      })
+      
+      beforeEach(function(){
+        process.env.MONGO_AUTH = false;
+      });
+
+      it('calls DB.connect', async () => {
+        
+        const connectSpy = jest.spyOn(MockDB, 'connect');
+        
+        await MockDB.connect();
+        expect(connectSpy).toHaveBeenCalledTimes(1);
+      });
+
+      it('gets and logs dbs', async () => { 
+        let dbs = await MockDB.getAndLogDBs()
+        console.log('dbs')
+        console.log(dbs)
+        expect(console.table).toHaveBeenCalledTimes(1)
+      })
+
+      it('calls DB.close', async () => {
+        const closeSpy = jest.spyOn(MockDB, 'close');
+        await MockDB.close();
+        expect(closeSpy).toHaveBeenCalledTimes(1);
+      });
+    })
+  })
+
 })
