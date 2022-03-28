2ec2c409851e2ab380d50ef3fd145b722792fff9
Eric Laursen <ericjlaursen@gmail.com>
2022-03-28 14:44:22 -0400

    new methods, connect, close and getAndLogDBs

--- server/models/db/index.js
+++ server/models/db/index.js
@@ -1,8 +1,52 @@
+const { MongoClient } = require('mongodb');
+const { GLOBAL_STATE } = require('./../../global');
 class DB{
-  constructor({mongoClient, db}){
-    this.client = mongoClient;
-    this.db = db;
+  constructor({ connectionObj, dbName }) {
+    this.connectionObj = connectionObj;
+    this.client = null;
+    this.dbName = dbName;
   }
+
+  /*
+    takes db connection obj
+      username
+      pw
+      host
+      port
+      authDB
+    returns the client
+  */ 
+  async connect() {
+    try {      
+      // Connect
+      const uriStr = require('./../../database').makeConnectionString(this.connectionObj)
+      this.client = new MongoClient(uriStr);
+      await this.client.connect();
+
+      // store 
+      GLOBAL_STATE.DB_CONNECTED = true;
+      console.log(`SERVER: Connected to mongo db on ${this.connectionObj.host}:${this.connectionObj.port}`)
+      // await setupStores(mongoClient);
+
+      return this.client;
+    } catch (e) {
+      console.log(`DB Class connect method error:`)
+      console.log(e);
+    }
+  }
+
+  close() {
+    this.client.close();
+    console.log(`CLOSED db connection on ${this.connectionObj.host}:${this.connectionObj.port}`)
+    
+  }
+
+  async getAndLogDBs() {
+    let databasesList = await this.client.db().admin().listDatabases();
+    const { databases } = databasesList
+    console.table(databases)
+    return databases;
+  };
 }
 
 module.exports = {
