65b91e2cb872c36e6fa6e5f4e5555b89cd203422
Eric Laursen <ericjlaursen@gmail.com>
2022-03-28 16:07:52 -0400

    passes dbName to setupDB, comments setupDB

--- server/index.js
+++ server/index.js
@@ -5,19 +5,21 @@ const {
   startServer 
 } = require('./server-setup')
 const { ServicesEmitter } = require('./global')
-const { setupDB } = require('./db')
 
 async function startApi(){
   try{
     startServer(expressObj)
-    if(!process.env.DB || process.env.DB === true){
-      await setupDB({
+
+    if (!process.env.DB || process.env.DB === true) {
+      const db_obj = {
         username: process.env.MONGO_DB_USER,
         pw: process.env.MONGO_DB_PW,
         host: process.env.MONGO_DB_HOST,
         port: process.env.MONGO_DB_PORT,
-        authDB: process.env.MONGO_DB_AUTH_DB
-      })
+        authDB: process.env.MONGO_DB_AUTH_DB,
+        dbName: 'SayWhat'
+      }
+      // await setupDB({...db_obj})
     }
   }catch(e){
     console.log(e)
