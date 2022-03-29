fe5cd7891ca1c1bccb40ba7572fce4395c992a9d
Eric Laursen <ericjlaursen@gmail.com>
2022-03-29 15:00:38 -0400

    only export the currently used model

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,10 @@
 const { DB } = require('./db');
-const { Crud } = require('./crud');
+// const { Crud } = require('./crud');
+
+// const Users = new Crud({ collection: 'Users' })
+
 module.exports = {
   DB,
-  Crud
+  // Crud
+  // Users
 }
