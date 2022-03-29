c81122a69d8b9aad37cfff205ad6b7f5c83700db
Eric Laursen <ericjlaursen@gmail.com>
2022-03-29 14:58:33 -0400

    changes global var name

--- server/global/state.js
+++ server/global/state.js
@@ -1,5 +1,6 @@
 const GLOBAL_STATE = {
-  DB_CONNECTED:false,   //used in checkForDbConnection middleware
+  MONGO_CONNECTED: false,   //used in checkForDbConnection middleware
+  MONGO_CLIENT: null,
   DBS:{
     SayWhat: false
   },
