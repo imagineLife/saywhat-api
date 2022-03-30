e14e06de7038c86dee0cc89ef2d4847a8f3979d2
Eric Laursen <ericjlaursen@gmail.com>
2022-03-29 21:19:34 -0400

    return keyword

--- server/routes/healthcheck/health-check.spec.js
+++ server/routes/healthcheck/health-check.spec.js
@@ -3,15 +3,17 @@ const chai= require('chai');
 const chaiHttp= require('chai-http');
 const { HEALTH_CHECK } = require('./../../global/constants');
 const { startServer, stopServer, expressObj } = require('./../../server-setup');
+
 describe(HEALTH_CHECK, function () {
   chai.use(chaiHttp);
   let localServerObj;
+
   beforeEach(async function () {
     localServerObj = await startServer(expressObj)
   });
 
   afterEach(async function () {
-    await stopServer(localServerObj)
+    return await stopServer(localServerObj)
   });
 
   it('get returns "server is up & running!"', async function () {
