eda26780333691d448cb5c562047b5042e052b34
Eric Laursen <ericjlaursen@gmail.com>
2022-03-31 13:39:14 -0400

    adjust expected mongoConnection shape

--- server/server-setup/setup-fns.spec.js
+++ server/server-setup/setup-fns.spec.js
@@ -14,7 +14,7 @@ describe('server-setup setup-fns', () => {
     it('calls DB model with success', async () => {
       process.env.MONGO_AUTH = false;
       mongoConnection = await setupDB({ host: 'localhost', port: '27017' })
-      expect(mongoConnection.s.url).toBe("mongodb://localhost:27017/?connectTimeoutMS=2500");
+      expect(mongoConnection.client.s.url).toBe("mongodb://localhost:27017/?connectTimeoutMS=2500");
     });
   })
 })
