5f4c961135d4e8a16bb740290fcb0a78b915ec76
Eric Laursen <ericjlaursen@gmail.com>
2022-04-02 19:33:58 -0400

    more tests

--- server/models/crud/crud-model.spec.js
+++ server/models/crud/crud-model.spec.js
@@ -1,73 +1,67 @@
 const { Crud } = require('.');
+const { setupDB } = require('./../../server-setup');
 
 describe('CRUD Model', () => {
-  // describe('throws err', () => { 
-  //   it('without mongoClient', () => { 
-  //     expect(() => { 
-  //       new Crud({ db: 'qwer', collection: 'horse'})
-  //     }).toThrow('Cannot call Crud without client')
-  //   })
-  //   it('with mongoClient, without db', () => { 
-  //     expect(() => { 
-  //       new Crud({mongoClient: 'qwer', collection: 'horse'})
-  //     }).toThrow('Cannot call Crud without db')
-  //   })
-  // })
 
-  describe('works', () => { 
-    let testDB;
-    // beforeAll(() => {
-
-    // })
-
-    // afterAll(() => { 
-
-    // })
-    const mockCollFn = jest.fn()
-    const mockDb = {
-      collection: mockCollFn
-    }
-    const Cat = new Crud({ db: mockDb, collection: 'TestCollection' })
-    
-    const catKeys = Object.getOwnPropertyNames(Cat)
-    const expectedKeys = ['connectionObj', 'db', 'collection']
-
-    it('returns matching collection name from Model input', () => { 
-      expect(Cat.collectionName).toBe('poiu')
+  describe('Crud Model', () => { 
+    let TestMongoClient;
+    let TestSayWhat;
+    let Cat;
+    let testCreatedObject;
+    beforeAll(async () => {
+      process.env.MONGO_AUTH = false;
+      const db_obj = {
+        host: 'localhost',
+        port: '27017'
+      }
+      TestMongoClient = await setupDB({ ...db_obj })
+      TestSayWhat = TestMongoClient.registerDB('TestSayWhat')
+      Cat = new Crud({ db: TestSayWhat, collection: 'TestCollection' })
     })
 
-    describe('builds obj with expected keys', () => { 
-      it.each(expectedKeys)(`%s`, (xKey) => { 
-        expect(catKeys.includes(xKey)).toBe(true)
-      })
+    afterAll(async () => { 
+      await TestMongoClient.close()
     })
 
-    let thisTestObj;
+    it('Crud.collectionName matches input param', () => { 
+      expect(Cat.collectionName).toBe('TestCollection')
+    })
 
-    describe('createOne', () => {
+    const expectedKeys = [ 'connectionObj', 'client', 'db', 'collectionName', 'collection' ]
+    it.each(expectedKeys)(`%s key is present`, (xKey) => { 
+      const catKeys = Object.getOwnPropertyNames(Cat)
+      expect(catKeys.includes(xKey)).toBe(true)
+    })
+
+    it('createOne then readOne works with matching _id', async () => {
       let testObj = { dog: 'horse' }
-      let createOneRes;
-      it('returns obj with keys', async () => { 
-        createOneRes = await Cat.createOne(testObj)
-        console.log('createOneRes')
-        console.log(createOneRes)
-        expect(1).toBe(2)
+      testCreatedObject = await Cat.createOne(testObj)
+      let testFoundObj = await Cat.readOne({_id: testCreatedObject.insertedId})
+      expect(testCreatedObject.insertedId.toString()).toBe(testFoundObj._id.toString())
+    })
+
+    describe('errors throw with disconnected DB', () => { 
+      beforeAll(async () => { 
+        await TestMongoClient.close();
+      })
+
+      it('createOne', async () => {
+        let testObj = { failable: 'obj' }
+        
+        try {
+          await Cat.createOne(testObj)
+        } catch (e) { 
+          expect(e.message).toBe('MongoNotConnectedError: MongoClient must be connected to perform this operation')
+        }
+      })
+
+      it('readOne', async () => {
+        try {
+          await Cat.readOne({_id: testCreatedObject.insertedId})
+        } catch (e) { 
+          expect(e.message).toBe('MongoNotConnectedError: MongoClient must be connected to perform this operation')
+        }
       })
     })
-    // it('read: returns string', () => {
-    //   let testObj = { dog: 'horse' }
-    //   let res = Cat.read(testObj)
-    //   expect(res).toBe(`Getting on poiu where ${JSON.stringify(testObj)}`)
-    // })
-    // it('update: returns string', () => {
-    //   let testObj = { dog: 'horse' }
-    //   let res = Cat.update(testObj)
-    //   expect(res).toBe(`Updating on poiu where ${JSON.stringify(testObj)}`)
-    // })
-    // it('read: returns string', () => {
-    //   let testObj = { dog: 'horse' }
-    //   let res = Cat.delete(testObj)
-    //   expect(res).toBe(`deleting on poiu where ${JSON.stringify(testObj)}`)
-    // })
   })
 })
