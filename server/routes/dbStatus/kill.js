a46484e2db1a4f1d508368a429cb41f47c73e94c
Eric Laursen <ericjlaursen@gmail.com>
2022-04-03 11:25:48 -0400

    tests against global mongo_client state

--- server/routes/dbStatus/kill.js
+++ server/routes/dbStatus/kill.js
@@ -1,9 +1,10 @@
 const { ServicesEmitter } = require('./../../global/events')
 const GLOBAL_STATE = require('./../../global/state');
 
-function killHandler(req,res,nxt){
+async function killHandler(req,res,nxt){
   ServicesEmitter.emit('DB_DISCONNECT', false)
-  res.status(200).send({MONGO_CONNECTED: GLOBAL_STATE.MONGO_CONNECTED})
+  await GLOBAL_STATE.MONGO_CLIENT.topology.close()
+  res.status(200).send({MONGO_CONNECTED: GLOBAL_STATE.MONGO_CLIENT.topology.isConnected()})
 }
 
 module.exports = killHandler;
