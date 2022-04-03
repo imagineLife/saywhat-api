a9d5974befe249d3ef690f5ddfa614f895d732c8
Eric Laursen <ericjlaursen@gmail.com>
2022-04-03 11:26:29 -0400

    starts db + tests more

--- server/routes/dbStatus/db-status.spec.js
+++ server/routes/dbStatus/db-status.spec.js
@@ -2,11 +2,15 @@
 const chai= require('chai');
 const chaiHttp= require('chai-http');
 const { routes: { DB } } = require('./../../global/constants');
-const { startServer, stopServer, expressObj } = require('./../../server-setup');
+const GLOBAL_STATE = require('./../../global/state')
+const { startServer, stopServer, expressObj, setupDB } = require('./../../server-setup');
+// const { MongoClient } = require('mongodb')
 describe(DB.ROOT, function () {
   chai.use(chaiHttp);
   let localServerObj;
-  beforeEach(async function () {
+  let TestMongoClient;
+  beforeAll(async function () {
+    process.env.MONGO_AUTH = false;
     if (localServerObj && localServerObj.close) {
       await stopServer(localServerObj) 
     }
@@ -14,9 +18,14 @@ describe(DB.ROOT, function () {
       await stopServer(expressObj)
     }
     localServerObj = await startServer(expressObj)
+    const db_obj = {
+      host: 'localhost',
+      port: '27017'
+    }
+    TestMongoClient = await setupDB({ ...db_obj });
   });
 
-  afterEach(async function () {
+  afterAll(async function () {
     if (localServerObj && localServerObj.close) {
       await stopServer(localServerObj) 
     }
@@ -25,16 +34,24 @@ describe(DB.ROOT, function () {
     }
   });
 
-  it(`${DB.KILL} returns down val`, async function () {
-    const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.KILL}`);
-    expect(JSON.stringify(res.body)).toBe(JSON.stringify({MONGO_CONNECTED: false}));
-  });
-  it(`${DB.RESTART} returns up val`, async function () {
-    const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.RESTART}`);
-    expect(JSON.stringify(res.body)).toBe(JSON.stringify({MONGO_CONNECTED: true}));
-  });
   it(`${DB.STATUS} returns db Status obj`, async function () {
     const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.STATUS}`);
     expect(Object.keys(res.body)[0]).toBe('MONGO_CONNECTED');
   });
+  it(`${DB.KILL} returns down val`, async function () {
+    const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.KILL}`);
+    expect(JSON.stringify(res.body)).toBe(JSON.stringify({MONGO_CONNECTED: false}));
+  });
+  describe(`${DB.RESTART}`, () => {
+    it(`returns up val`, async function () {
+      const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.RESTART}`);
+      expect(JSON.stringify(res.body)).toBe(JSON.stringify({ MONGO_CONNECTED: true }));
+    });
+
+    it('throws err when db is disconnected', async () => {
+      await TestMongoClient.close();
+      const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.RESTART}`);
+      expect(JSON.stringify(res.body)).toBe(JSON.stringify({ Error: 'server error' }));
+    })
+  })
 });
