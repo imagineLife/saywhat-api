a7c1bb29e3fb126b6db2776c38359502a63f6ab5
Eric Laursen <ericjlaursen@gmail.com>
2022-04-03 21:35:53 -0400

    de-nest tests + close mongo connection

--- server/routes/dbStatus/db-status.spec.js
+++ server/routes/dbStatus/db-status.spec.js
@@ -27,12 +27,14 @@ describe(DB.ROOT, function () {
   });
 
   afterAll(async function () {
+    await TestMongoClient.close()
     if (localServerObj && localServerObj.close) {
       await stopServer(localServerObj) 
     }
     if (expressObj && expressObj.close) { 
       await stopServer(expressObj)
     }
+    await TestMongoClient.close()
   });
 
   it(`${DB.STATUS} returns db Status obj`, async function () {
@@ -43,19 +45,50 @@ describe(DB.ROOT, function () {
     const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.KILL}`);
     expect(JSON.stringify(res.body)).toBe(JSON.stringify({MONGO_CONNECTED: false}));
   });
-  describe(`${DB.RESTART}`, () => {
-    it(`returns up val`, async function () {
-      const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.RESTART}`);
-      expect(JSON.stringify(res.body)).toBe(JSON.stringify({ MONGO_CONNECTED: true }));
-    });
-
-    it('throws err when GLOBAL mongo client is missing', async () => {
-      let tempClient = GLOBAL_STATE.MONGO_CLIENT;
-      GLOBAL_STATE.MONGO_CLIENT = null;
-      const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.RESTART}`);
-      GLOBAL_STATE.MONGO_CLIENT = tempClient;
-      expect(res.status).toBe(500)
-      expect(JSON.stringify(res.body)).toBe(JSON.stringify({ Error: 'server error' }));
-    })
-  })
 });
+
+describe(`${DB.RESTART}`, () => {
+    chai.use(chaiHttp);
+  let localServerObj;
+  let TestMongoClient;
+  beforeEach(async function () {
+    process.env.MONGO_AUTH = false;
+    if (localServerObj && localServerObj.close) {
+      await stopServer(localServerObj) 
+    }
+    if (expressObj && expressObj.close) { 
+      await stopServer(expressObj)
+    }
+    localServerObj = await startServer(expressObj)
+
+    const db_obj = {
+      host: 'localhost',
+      port: '27017'
+    }
+    TestMongoClient = await setupDB({ ...db_obj });
+  });
+
+  afterEach(async function () {
+    if (localServerObj && localServerObj.close) {
+      await stopServer(localServerObj) 
+    }
+    if (expressObj && expressObj.close) { 
+      await stopServer(expressObj)
+    }
+
+    await TestMongoClient.close()
+  });
+  it(`returns up val`, async function () {
+    const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.RESTART}`);
+    expect(JSON.stringify(res.body)).toBe(JSON.stringify({ MONGO_CONNECTED: true }));
+  });
+
+  it('throws err when GLOBAL mongo client is missing', async () => {
+    let tempClient = GLOBAL_STATE.MONGO_CLIENT;
+    GLOBAL_STATE.MONGO_CLIENT = null;
+    const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.RESTART}`);
+    GLOBAL_STATE.MONGO_CLIENT = tempClient;
+    expect(res.status).toBe(500)
+    expect(JSON.stringify(res.body)).toBe(JSON.stringify({ Error: 'server error' }));
+  })
+})
