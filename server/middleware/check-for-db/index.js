5c60db7918056dc33a88a7dd0473296bcb6b2408
Eric Laursen <ericjlaursen@gmail.com>
2022-04-03 20:25:51 -0400

    uses topology instead of static var

--- server/middleware/check-for-db/index.js
+++ server/middleware/check-for-db/index.js
@@ -3,7 +3,7 @@ const { GLOBAL_STATE } = require('./../../global')
 function checkForDbConnection(req,res,nxt){  
   const notDB = !req.path.match('^/db')
   const notHealthCheck = !req.path.match('^/health-check')
-  const DB_NOT_CONNECTED = GLOBAL_STATE.MONGO_CONNECTED !== true;
+  const DB_NOT_CONNECTED = GLOBAL_STATE?.MONGO_CLIENT?.topology?.isConnected() !== true;
   if( notDB && notHealthCheck && DB_NOT_CONNECTED ){
     return res.status(200).send({Error: "Server Error, try again shortly"})
   }
