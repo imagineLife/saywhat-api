bcc878b4134e372edd75af4de690606239647693
Eric Laursen <ericjlaursen@gmail.com>
2022-04-03 20:26:21 -0400

    init speeches route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,14 +4,16 @@ const {
   routes: { 
     DB,
     HEALTH_CHECK,
+    SPEECHES
     // USERS
   }
 } = require('./../global/constants');
 const healthCheckHandler =  require('./healthcheck');
 const dbStatusHandler = require('./dbStatus');
+const speechesHandler = require('./speeches')
 
 router.use(DB.ROOT, dbStatusHandler)
 router.use(HEALTH_CHECK, healthCheckHandler)
-
+router.use(SPEECHES.ROOT, speechesHandler)
 
 module.exports = router;
