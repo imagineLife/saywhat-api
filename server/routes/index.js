9050955dd34be76e9a9eda5218c75edb205418c8
Eric Laursen <ericjlaursen@gmail.com>
2022-04-14 20:28:01 -0400

    init user routes + NOTES

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,15 +4,35 @@ const {
   routes: { 
     DB,
     HEALTH_CHECK,
-    SPEECHES
+    SPEECHES,
+    USERS
   }
 } = require('./../global/constants');
 const healthCheckHandler =  require('./healthcheck');
 const dbStatusHandler = require('./dbStatus');
 const speechesHandler = require('./speeches')
+const usersHandler = require('./users')
 
 router.use(DB.ROOT, dbStatusHandler)
 router.use(HEALTH_CHECK, healthCheckHandler)
 router.use(SPEECHES.ROOT, speechesHandler)
+router.use(USERS.ROOT, usersHandler)
+/*
+  /users
+    get       - get list  
+    post      - create one
+
+    /:id
+      get     - get info  re:user
+      patch   - update    re:user
+    
+      /auth
+        post  - first "create account"
+        patch - user req.params
+          ?field - update/set field
+
+
+
+*/ 
 
 module.exports = router;
