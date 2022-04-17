7d91d452eb9d7360e30c84c7a0b13c8911431219
Eric Laursen <ericjlaursen@gmail.com>
2022-04-16 22:15:46 -0400

    dummy postUsers handler

--- server/routes/users/root/post/index.js
+++ server/routes/users/root/post/index.js
@@ -1,5 +1,22 @@
-function postUsers(req, res, next) { 
-  return res.status(200).send('post users')
+/*
+  Dependencies
+*/ 
+const {
+  GLOBAL_STATE: { 
+    Collections
+  } 
+ } = require('./../../../../global')
+
+async function postUsers(req, res, next) { 
+  try {
+    let { Users } = Collections;
+    let createdUser = await Users.createOne({ asdf: 'sdfg' })
+    return res.status(200).json({works: 'qwer'})
+  } catch (e) { 
+    console.log('e')
+    console.log(e)
+    res.status(500).json({Error: 'postUsers error'})
+  }
 }
 
 module.exports = postUsers;
