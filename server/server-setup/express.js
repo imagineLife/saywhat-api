8709b08514863ae0a839bb3e631d1fbb4ffaed7c
Eric Laursen <ericjlaursen@gmail.com>
2022-04-19 08:07:59 -0400

    new middleware who dis

--- server/server-setup/express.js
+++ server/server-setup/express.js
@@ -5,6 +5,7 @@
 */ 
 const express = require('express');
 const rootRouter = require('./../routes')
+const bodyParser = require('body-parser')
 const { GLOBAL_STATE, ServicesEmitter } = require('./../global');
 const { checkForDbConnection } = require('./../middleware')
 const STATIC_DIR = './../static';
@@ -27,9 +28,14 @@ expressObj.use(express.static(STATIC_DIR));
 
 /*
 
-  setup express Route Handling
+  setup all-route middleware
 
 */ 
+// parse application/x-www-form-urlencoded
+expressObj.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+expressObj.use(bodyParser.json())
 expressObj.use(checkForDbConnection)
 
 
