6c63b0635492ff40f25679f92fb4ed5d39e75a5c
Eric Laursen <ericjlaursen@gmail.com>
2022-04-19 20:46:02 -0400

    update catch logs

--- server/routes/users/root/post/index.js
+++ server/routes/users/root/post/index.js
@@ -13,8 +13,8 @@ async function postUsers(req, res, next) {
     let createdUser = await Users.createOne({ asdf: 'sdfg' })
     return res.status(200).json({works: 'qwer'})
   } catch (e) { 
-    console.log('e')
-    console.log(e)
+    console.log('postUsers error:')
+    console.log(e.message)
     res.status(500).json({Error: 'postUsers error'})
   }
 }
