e857f325affbaf7656c72c6102a098813df98e89
Eric Laursen <ericjlaursen@gmail.com>
2022-04-24 21:10:56 -0400

    more tests

--- server/routes/speeches/root/post/post-speeches.spec.js
+++ server/routes/speeches/root/post/post-speeches.spec.js
@@ -2,12 +2,17 @@
 const chai = require('chai');
 const chaiHttp= require('chai-http');
 const { routes: { SPEECHES: { ROOT } } } = require('./../../../../global/constants');
-const GLOBAL_STATE = require('./../../../../global/state')
+const { GLOBAL_STATE: { Collections } } = require('./../../../../global');
 const { startServer, stopServer, expressObj, setupDB } = require('./../../../../server-setup');
+const { Crud } = require('./../../../../models');
 describe(`${ROOT}: POST`, function () {
   chai.use(chaiHttp);
   let localServerObj;
   let TestMongoClient;
+  let TestSpeechesCollection;
+  const DB_NAME = 'TestSayWhat';
+  const COLL_NAME = 'TestSpeeches';
+  
   
   beforeAll(async () => { 
     process.env.MONGO_AUTH = false;
@@ -16,33 +21,66 @@ describe(`${ROOT}: POST`, function () {
       port: '27017'
     }
     TestMongoClient = await setupDB({ ...db_obj });
+     if (localServerObj && localServerObj.close) {
+       await stopServer(localServerObj);
+     }
+     if (expressObj && expressObj.close) {
+       await stopServer(expressObj);
+     }
+    localServerObj = await startServer(expressObj);
+    TestSayWhat = TestMongoClient.registerDB(DB_NAME);
+    TestSpeechesCollection = new Crud({ db: TestSayWhat, collection: COLL_NAME });
+    Collections.Speeches = TestSpeechesCollection;
   })
 
   afterAll(async () => { 
+    Collections.Speeches = null;
     await TestMongoClient.close()
+    if (localServerObj && localServerObj.close) {
+      await stopServer(localServerObj);
+    }
+    if (expressObj && expressObj.close) {
+      await stopServer(expressObj);
+    }
   })
 
-  beforeEach(async () => {
-    if (localServerObj && localServerObj.close) {
-      await stopServer(localServerObj) 
-    }
-    if (expressObj && expressObj.close) { 
-      await stopServer(expressObj)
-    }
-    localServerObj = await startServer(expressObj)
-  });
-
-  afterEach(async () => {
-    if (localServerObj && localServerObj.close) {
-      await stopServer(localServerObj) 
-    }
-    if (expressObj && expressObj.close) { 
-      await stopServer(expressObj)
-    }
-  });
-
-  it(`returns horse`, async () => {
-    const res = await chai.request(localServerObj).post(`${ROOT}`);
-    expect(res.body).toBe("posting a speech here, see server logs for more deets")
-  });
+  describe('fails', () => { 
+    describe('without expected params', () => { 
+      it('no orator', async () => { 
+        const res = await chai.request(localServerObj).post(`${ROOT}`).send({
+          date: '1234',
+          text: 'qwer'
+        });
+        expect(res.status).toBe(422);
+        expect(JSON.stringify(res.body)).toBe(JSON.stringify({ Error: 'missing required params' }));
+      })
+      it('no date', async () => {
+        const res = await chai.request(localServerObj).post(`${ROOT}`).send({
+          orator: '1234',
+          text: 'qwer',
+        });
+        expect(res.status).toBe(422);
+        expect(JSON.stringify(res.body)).toBe(JSON.stringify({ Error: 'missing required params' }));
+      });
+      it('no text', async () => {
+        const res = await chai.request(localServerObj).post(`${ROOT}`).send({
+          orator: '1234',
+          date: 'qwer',
+        });
+        expect(res.status).toBe(422);
+        expect(JSON.stringify(res.body)).toBe(JSON.stringify({ Error: 'missing required params' }));
+      });
+    })
+  })
+  describe('succeeds', () => { 
+    it(`returns 200 & success body`, async () => {
+      const res = await chai.request(localServerObj).post(`${ROOT}`).send({
+        orator: '1234',
+        date: 'qwer',
+        text: '1234'
+      });
+      expect(res.status).toBe(200)
+      expect(res.body).toBe('success');
+    });
+  })
 });
