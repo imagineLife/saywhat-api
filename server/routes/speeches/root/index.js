8bc176ae6e55e8cb9f4004c08f18bf4ec4b87ff6
Eric Laursen <ericjlaursen@gmail.com>
2022-04-24 21:10:01 -0400

    assertParams on post

--- server/routes/speeches/root/index.js
+++ server/routes/speeches/root/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const getSpeeches = require('./get');
 const postASpeech = require('./post');
+const { assertParams } = require('./../../../middleware')
 
 // summary of speeches
 router.get(`/`, getSpeeches)
-router.post('/', postASpeech);
+router.post('/', assertParams({body: ['orator', 'date', 'text']}), postASpeech);
 
 module.exports = router;
