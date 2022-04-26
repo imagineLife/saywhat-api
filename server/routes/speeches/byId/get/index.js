3a2a26f4bc4102baac1449d10794217779a65bd1
Eric Laursen <ericjlaursen@gmail.com>
2022-04-26 19:24:47 -0400

    include readOne functionality & returing object

--- server/routes/speeches/byId/get/index.js
+++ server/routes/speeches/byId/get/index.js
@@ -1,7 +1,17 @@
-function getById(req, res, next) { 
-  console.log('getById: req.params')
-  console.log(req.params)
-  return res.status(200).json(`getById: ${req.params.speechId}`)
+const { GLOBAL_STATE: { Collections } } = require('./../../../../global')
+const Oid = require('mongodb').ObjectId;
+async function getById(req, res, next) { 
+  try { 
+    
+    const { speechId } = req.params
+    const dbRes = await Collections.Speeches.readOne({ _id: Oid(speechId) });
+    delete dbRes._id;
+    return res.status(200).json(dbRes);
+  } catch (e) {
+    console.log('getById Error')
+    console.log(e);
+    return res.status(500).json({Error: 'get speech by id'})
+  }
 }
 
 module.exports = getById;
