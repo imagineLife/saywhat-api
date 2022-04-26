846d5197d4a05425501b6de5f26f1a70f35e8964
Eric Laursen <ericjlaursen@gmail.com>
2022-04-26 19:25:38 -0400

    more

--- server/routes/speeches/byId/get/get-by-speech-id.spec.js
+++ server/routes/speeches/byId/get/get-by-speech-id.spec.js
@@ -2,49 +2,73 @@
 const chai = require('chai');
 const chaiHttp= require('chai-http');
 const { routes: { SPEECHES: { ROOT } } } = require('./../../../../global/constants');
-const GLOBAL_STATE = require('./../../../../global/state')
+const { GLOBAL_STATE } = require('./../../../../global')
 const { startServer, stopServer, expressObj, setupDB } = require('./../../../../server-setup');
+const { Crud } = require('../../../../models');
 // const { MongoClient } = require('mongodb')
 describe(`${ROOT}:/speechId : GET`, function () {
   chai.use(chaiHttp);
+  let TestMongoClient;
   let localServerObj;
-  // let TestMongoClient;
-  beforeEach(async function () {
+  let TestSpeechCollection;
+  beforeAll(async function () {
     process.env.MONGO_AUTH = false;
     if (localServerObj && localServerObj.close) {
-      await stopServer(localServerObj) 
+      await stopServer(localServerObj);
     }
-    if (expressObj && expressObj.close) { 
-      await stopServer(expressObj)
+    if (expressObj && expressObj.close) {
+      await stopServer(expressObj);
     }
-    localServerObj = await startServer(expressObj)
+    localServerObj = await startServer(expressObj);
 
     const db_obj = {
       host: 'localhost',
-      port: '27017'
-    }
+      port: '27017',
+    };
     TestMongoClient = await setupDB({ ...db_obj });
+
+    TestSayWhat = TestMongoClient.registerDB('TestSayWhat');
+    TestSpeechCollection = new Crud({
+      db: TestSayWhat,
+      collection: 'TestSpeeches',
+    });
+    GLOBAL_STATE.Collections.Speeches = TestSpeechCollection;
   });
 
-  afterEach(async function () {
-    // await TestMongoClient.close()
+  afterAll(async function () {
     if (localServerObj && localServerObj.close) {
-      await stopServer(localServerObj) 
+      await stopServer(localServerObj);
     }
-    if (expressObj && expressObj.close) { 
-      await stopServer(expressObj)
+    if (expressObj && expressObj.close) {
+      await stopServer(expressObj);
     }
-    await TestMongoClient.close()
+    GLOBAL_STATE.Collections.Speeches = false;
+    await TestMongoClient.close();
   });
 
-  const testIdParams = [123, 234, 345]
-  it.each(testIdParams)(`returns text based on param %d`, async (paramVal) => {
-    const res = await chai.request(localServerObj).get(`${ROOT}/${paramVal}`);
-    expect(res.body).toBe(`getById: ${paramVal}`)
-  })
-  
-  // it(`${DB.KILL} returns down val`, async function () {
-  //   const res = await chai.request(localServerObj).get(`${DB.ROOT}${DB.KILL}`);
-  //   expect(JSON.stringify(res.body)).toBe(JSON.stringify({MONGO_CONNECTED: false}));
-  // });
+  it('successfully gets speech by speechID after speech insert', async () => {
+    let insertedSpeech;
+    const mockSpeech = {
+      orator: 'Test User',
+      date: '1234-23-2345',
+      text: 'This is the song that never ends',
+    };
+    try {
+      console.log('IN TRY')
+      
+      // insert
+      const { insertedId } = await TestSpeechCollection.createOne(mockSpeech);
+      insertedSpeech = insertedId;
+      const reqURL = `${ROOT}/${insertedId}`;
+      const apiRes = await chai.request(localServerObj).get(reqURL);
+      expect(apiRes.body.orator).toBeTruthy();
+      expect(apiRes.body.date).toBeTruthy();
+      expect(apiRes.body.text).toBeTruthy();
+    } catch (e) {
+      throw new Error(e)
+    } finally {
+      await TestSpeechCollection.remove()
+    }
+  });
 });
+
